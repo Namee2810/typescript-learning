f86685418464929afa45ee7f18faf65408c4bde1
Namee2810 <dvn281002@gmail.com>
2021-07-17 19:47:27 +0700

    signature function

---
+++ index.js
@@ -0,0 +1,3 @@
+var greetUser = function (name) { return console.log("Hello " + name); };
+var squareNumber = function (number) { return Math.pow(number, 2); };
+console.log(squareNumber(2));
---
+++ index.ts
@@ -0,0 +1,4 @@
+const greetUser = (name: string): void => console.log(`Hello ${name}`)
+
+const squareNumber = (number: number): number => number ** 2
+console.log(squareNumber(2))
