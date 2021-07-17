3a959b60695886a4bc4b2fb101ff77cb3177ab25
Namee2810 <dvn281002@gmail.com>
2021-07-17 17:24:05 +0700

    basic types

---
+++ basicTypes.ts
@@ -0,0 +1,5 @@
+let userName: string = "Nam"
+let age: number = 18
+let isMale: boolean = true
+//let skills: Array<string> = ["JS", "HTML/CSS", "React"]
+let skills: string[] = ["JS", "HTML/CSS", "React"]
