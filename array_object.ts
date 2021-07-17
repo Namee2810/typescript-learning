314942c7e01609bb4e5ce3f389d8079392c7b64d
Namee2810 <dvn281002@gmail.com>
2021-07-17 18:01:47 +0700

    array and object

---
+++ array_object.ts
@@ -0,0 +1,9 @@
+let skills: string[] = ["JS", "HTML/CSS", "React"]
+let numbers: Array<number> = [1, 2, 3]
+let mixed = ["Hi", 1, true]
+
+let person = {
+  name: "Nam",
+  age: 18,
+  skills
+}
--- basicTypes.ts
+++ basicTypes.ts
@@ -1,5 +1,3 @@
 let userName: string = "Nam"
 let age: number = 18
 let isMale: boolean = true
-//let skills: Array<string> = ["JS", "HTML/CSS", "React"]
-let skills: string[] = ["JS", "HTML/CSS", "React"]
